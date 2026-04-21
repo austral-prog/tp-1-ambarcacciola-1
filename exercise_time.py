0266f5f6c7bb69508021746d5f820bc280cfb896
ambarcacciola <acacciola@mail.austral.edu.ar>
2026-04-21 11:44:00 -0300

    Update exercise_time.py

--- exercise_time.py
+++ exercise_time.py
@@ -1,16 +1,14 @@
 def time():
-    """
-    Ejercicio 4 - Calculadora de Tiempo
+    total_segundos = 3665
 
-    Dado un total de segundos, calcular e imprimir:
-    1. Horas completas
-    2. Minutos completos restantes
-    3. Segundos restantes
-    """
-    horas_completas = total_segundos // 3600
+    horas = total_segundos // 3600
     resto = total_segundos % 3600
-    minutos_completos_restantes = resto // 60
-    segundos_restantes = resto % 60
-    print(horas_completas)
-    print(minutos_completos_restantes)
-    print(segundos_restantes)
+
+    minutos = resto // 60
+    segundos = resto % 60
+
+    print(horas)
+    print(minutos)
+    print(segundos)
+time()
+
