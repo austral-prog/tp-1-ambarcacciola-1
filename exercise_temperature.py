70ee4950ae82c9818e01a18f257ac17330735ba9
ambarcacciola <acacciola@mail.austral.edu.ar>
2026-04-21 11:36:07 -0300

    Update exercise_temperature.py

--- exercise_temperature.py
+++ exercise_temperature.py
@@ -1,12 +1,7 @@
 def temperature():
-    """
-    Ejercicio 3 - Conversión de Temperatura
-
-    Dada una temperatura en Celsius, imprimir:
-    1. La temperatura en Fahrenheit (F = C × 9/5 + 32)
-    2. La temperatura original en Celsius
-    """
     celsius = 25
-    farenheit = (celsius * 1.8 + 32)
+    fahrenheit = celsius * 9/5 + 32
+    print(fahrenheit)
     print(celsius)
-    print(farenheit)
+
+temperature()
